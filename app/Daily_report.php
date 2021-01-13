b5f2667280304a0efbecc3be2d08091ae0db5457
shamimi367 <y.kbt.szs@gmail.com>
2021-01-13 13:38:59 +0900

    日報用のコントローラーを作成、ルーティンルーティングをメモ)

--- app/Daily_report.php
+++ app/Daily_report.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Daily_report extends Model
 {
-    //
+    use HasFactory;
+    //テーブル名
+    protected $table = '';
+
+    // 可変項目
+    protected $fillable =
+    [
+        //
+    ];
 }
---
+++ app/Http/Controllers/DailyReportController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DailyReportController extends Controller
+{
+    public function __construct()
+    {
+        // 日報表示の画面は基本的にLaravel標準の
+        // Authを通すのが一番簡単だと思います
+        // $this->middleware('auth');
+    }
+    // <<とりあえず日報作成でやりたい事を書いていきます>>
+    /**
+     * 日報一覧を表示する
+     */
+
+
+    /**
+     * 日報の個別記事を表示する
+     */
+
+
+    /**
+     * 日報登録画面を表示
+     */
+
+
+    /**
+     * 日報登録確認画面を表示
+     */
+
+
+    /**
+     * 日報登録を実行
+     */
+
+
+    /**
+     * 日報差戻しの編集画面を表示
+     */
+
+
+    /**
+     * 日報差戻しの編集を実行
+     */
+
+
+    /**
+     * 日報を削除する
+     */
+}

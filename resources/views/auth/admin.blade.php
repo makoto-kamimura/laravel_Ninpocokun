c733b3b485a7763e28b642a43a720b9afe1c629a
3b20 <77141330+3b20@users.noreply.github.com>
2021-01-27 14:48:41 +0900

    216_ユーザー管理画面_変数対応修正

--- resources/views/auth/admin.blade.php
+++ resources/views/auth/admin.blade.php
@@ -18,38 +18,24 @@
         <h1>@yield('title2')</h1>
         <table border="1" class="m0a tac approval">
             <tr> 
+                <th style="display:none">cd</th>
                 <th>社員コード</th>
                 <th>所属部門/所属課</th>
                 <th>名前</th>
                 <th>退職日</th>
                 <th>システム管理者</th>
             </tr>
-            @if (isset($msgs1, $msgs2, $msgs3))
-                <tr> 
-                @foreach ($msgs1 as $msg1)
-                    <td>{{$msg1}}</td>
-                @endforeach
-                </tr> 
-                <tr> 
-                @foreach ($msgs2 as $msg2)
-                    <td>{{$msg2}}</td>
-                @endforeach
-                </tr> 
-                <tr> 
-                @foreach ($msgs3 as $msg3)
-                    <td>{{$msg3}}</td>
-                @endforeach
-                </tr>
-                <tr> 
-                @foreach ($msgs4 as $msg4)
-                    <td>{{$msg4}}</td>
-                @endforeach
-                </tr> 
-                <tr> 
-                @foreach ($msgs5 as $msg5)
-                    <td>{{$msg5}}</td>
-                @endforeach
-                </tr> 
+            @if (isset($users))
+            @foreach ($users as $user)
+            <tr>
+                <td style="display:none">$user->user_cd</td>
+                <td>$user->user_disp_cd</td>
+                <td>$user->dep_div_name</td>
+                <td>$user->user_name</td>
+                <td>$user->taishoku_date</td>
+                <td>$user->sys_admin</td>
+            </tr>
+            @endforeach 
             @endif
         </table>
 	</section>

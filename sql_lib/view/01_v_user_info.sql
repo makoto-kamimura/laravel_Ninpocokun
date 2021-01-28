7f69204f7f6fb97a6d608bff8a77215e90230bd4
3b20 <77141330+3b20@users.noreply.github.com>
2021-01-28 18:55:29 +0900

    DBビュー「v_user_info」_「dep_div_name」がnullになるのを修正

--- sql_lib/view/01_v_user_info.sql
+++ sql_lib/view/01_v_user_info.sql
@@ -2,12 +2,16 @@ DROP VIEW IF EXISTS v_user_info;
 CREATE VIEW v_user_info AS
 SELECT  
 u.cd user_cd,
-concat(sei ,'　', mei) user_name,
+concat(sei ,' ', mei) user_name,
 dp.cd dep_cd,
 dp.name dep_name,
 di.cd div_cd,
 di.name div_name,
-concat(dp.name ,'　', di.name) dep_div_name,
+CASE WHEN di.name IS NOT NULL THEN
+concat(dp.name ,' ', di.name)
+ELSE
+dp.name
+END AS dep_div_name,
 u.taishoku_date,
 u.sys_admin
 FROM users u

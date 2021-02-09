99be81ca592e238e3cb1cc312b398478c6c52c68
usernamedesu <orb_pnc_ver922@yahoo.co.jp>
2021-02-09 15:31:28 +0900

    210209_05

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
     <p class="btn02"><a href="{{route('report.index')}}"></a></p>
     @endif
     @if (isset( Auth::user()->pos_cd ) && Auth::user()->pos_cd < 30)
-    <p class="btn03"><a href="{{route('report.approve')}}"></a></p>
+    <p class="btn0{{Auth::user()->pos_cd == 1 ? "5" : "3"}}"><a href="{{route('report.approve')}}"></a></p>
     @endif
     @if (isset( Auth::user()->sys_admin ) && Auth::user()->sys_admin == 1)
     <p class="btn04"><a href="{{route('user.admin')}}"></a></p>
